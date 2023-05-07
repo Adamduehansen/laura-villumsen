984f966cb99e15cb3c4633ff9ae7f81e70dc7577
Adam Due Hansen <adamduehan@gmail.com>
2023-05-07 09:36:03 +0200

    Adds correct icon for mobile navigation.

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/assets/main.js');
+  eleventyConfig.addPassthroughCopy('src/assets/add-icon.svg');
 
   return {
     dir: {
--- src/assets/main.js
+++ src/assets/main.js
@@ -1,10 +1,12 @@
-function onMenuToggleButtonClick() {
-  navigationMenu.classList.toggle('hidden');
-  header.classList.toggle('h-screen');
-}
-
 const header = document.querySelector('header');
 const button = document.querySelector('button');
 const navigationMenu = document.querySelector('nav');
+
+function onMenuToggleButtonClick() {
+  navigationMenu.classList.toggle('hidden');
+  header.classList.toggle('h-screen');
+  button.classList.toggle('rotate-45');
+  button.classList.toggle('rotate-0');
+}
 
 button.addEventListener('click', onMenuToggleButtonClick);
