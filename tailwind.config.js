466f53879cf7b641c2dbb766100f377871668c66
Adam Due Hansen <adamduehan@gmail.com>
2023-05-20 20:06:06 +0200

    Adds nextjs.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{njk,md}'],
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
-    fontFamily: {
-      syne: 'Syne, sans-serif;',
-      unbounded: 'Unbounded, sans-serif;',
-      neue: 'neue-haas-grotesk-display',
-      'owners-wide': 'owners-wide',
-    },
     extend: {
-      fontSize: {
-        header: '20rem',
-      },
-      colors: {
-        lime: {
-          450: '#96C92C',
-        },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
   plugins: [],
-  safelist: ['h-screen', 'translate-y-0', 'rotate-45'],
-};
+}
