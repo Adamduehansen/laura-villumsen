3fe6d58dd27d2942ff03fda4fd8ac7da80b2ecf9
Adam Due Hansen <adamduehan@gmail.com>
2023-05-23 19:52:29 +0200

    Adds hover effect on menu.

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/assets/main.js');
   eleventyConfig.addPassthroughCopy('src/assets/add-icon.svg');
+  eleventyConfig.addPassthroughCopy('src/assets/arrow_left.png');
+  eleventyConfig.addPassthroughCopy('src/assets/arrow_right.png');
 
   return {
     dir: {
