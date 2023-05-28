59c1dee5a5ba02ac894f250acccb306b13499e87
Adam Due Hansen <adamduehan@gmail.com>
2023-05-28 16:00:55 +0200

    Adds arrow right svg.

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/assets/main.js');
   eleventyConfig.addPassthroughCopy('src/assets/add-icon.svg');
   eleventyConfig.addPassthroughCopy('src/assets/arrow_left.svg');
-  eleventyConfig.addPassthroughCopy('src/assets/arrow_right.png');
+  eleventyConfig.addPassthroughCopy('src/assets/arrow_right.svg');
   eleventyConfig.addPassthroughCopy('src/robots.txt');
 
   return {
