eac9492c4f41c68ca73f47ed22559ad55a703672
Adam Due Hansen <adamduehan@gmail.com>
2023-05-28 15:06:31 +0200

    Hover arrows are now svgs.

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/assets/main.js');
   eleventyConfig.addPassthroughCopy('src/assets/add-icon.svg');
-  eleventyConfig.addPassthroughCopy('src/assets/arrow_left.png');
+  eleventyConfig.addPassthroughCopy('src/assets/arrow_left.svg');
   eleventyConfig.addPassthroughCopy('src/assets/arrow_right.png');
   eleventyConfig.addPassthroughCopy('src/robots.txt');
 
