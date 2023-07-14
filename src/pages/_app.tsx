e6c11564b7b91f3dc66534f09650a09f0d9c0d5e
Adam Due Hansen <adamduehan@gmail.com>
2023-07-14 14:57:54 +0200

    Adds font styles.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,24 @@ import type { AppProps } from 'next/app';
 import Header from '@/components/Header';
 import PageDataProvider from '@/components/PageDataProvider';
 import { PageProps } from '@/utils/models';
-import '@/styles/globals.css';
 import Footer from '@/components/Footer';
+import '@/styles/globals.css';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps<PageProps>) {
   return (
-    <PageDataProvider pageData={pageProps}>
-      <Header />
-      <Component {...pageProps} />
-      <Footer />
-    </PageDataProvider>
+    <>
+      <Head>
+        <link
+          rel='stylesheet'
+          href='https://use.typekit.net/wsq1sgw.css'
+        ></link>
+      </Head>
+      <PageDataProvider pageData={pageProps}>
+        <Header />
+        <Component {...pageProps} />
+        <Footer />
+      </PageDataProvider>
+    </>
   );
 }
