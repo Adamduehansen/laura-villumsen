bda9463ea1a67cea389db64557b0b59962cf0a0c
Adam Due Hansen <adamduehan@gmail.com>
2023-07-15 23:22:33 +0200

    Adds tests.

---
+++ src/components/Col.test.tsx
@@ -0,0 +1,27 @@
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Col from './Col';
+
+describe('Col', () => {
+  test('Should set "col-span-12" as default', () => {
+    // Arrange
+    const { container } = render(<Col>Any Children</Col>);
+
+    // Act
+    const div = container.firstChild;
+
+    // Assert
+    expect(div).toHaveClass('col-span-12');
+  });
+
+  test('should set "col-span" class from props.sm', () => {
+    // Arrange
+    const { container } = render(<Col sm={6}>Any Children</Col>);
+
+    // Act
+    const div = container.firstChild;
+
+    // Assert
+    expect(div).toHaveClass('col-span-6');
+  });
+});
--- src/components/Col.tsx
+++ src/components/Col.tsx
@@ -1,7 +1,9 @@
+type ColRange = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+
 type Props = React.PropsWithChildren<{
-  sm: number;
+  sm?: ColRange;
 }>;
 
-export default function Col({ children, sm }: Props): JSX.Element {
+export default function Col({ children, sm = 12 }: Props): JSX.Element {
   return <div className={`col-span-${sm}`}>{children}</div>;
 }
