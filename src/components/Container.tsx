25539c8c0b1441ef3f80751e799d59d0728fec58
Adam Due Hansen <adamduehan@gmail.com>
2023-07-15 10:47:27 +0200

    Adds sm menu button to Header.

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,5 +1,11 @@
+type Props = React.PropsWithChildren<{
+  as?: keyof HTMLElementTagNameMap;
+}>;
+
 export default function Container({
   children,
-}: React.PropsWithChildren): JSX.Element {
-  return <div className='mx-grid'>{children}</div>;
+  as = 'div',
+}: Props): JSX.Element {
+  const Component = as;
+  return <Component className='mx-grid'>{children}</Component>;
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,23 @@
 import Link from 'next/link';
 import { useContext } from 'react';
 import { PageDataContext } from './PageDataProvider';
-import Navigation from './Navigation';
+import Image from 'next/image';
+import Container from './Container';
 
 export default function Header(): JSX.Element {
   const { siteName, subName } = useContext(PageDataContext);
 
   return (
-    <header>
-      <Link href='/' className='block'>
-        <div>{siteName}</div>
-        <div>{subName}</div>
-      </Link>
-      <Navigation />
-    </header>
+    <Container as='header'>
+      <div className='flex justify-between items-center mt-4 mb-6'>
+        <Link href='/' className='block'>
+          <div>{siteName}</div>
+          <div>{subName}</div>
+        </Link>
+        <button className='w-5 h-5'>
+          <Image src='add.svg' alt='' width={20} height={20} />
+        </button>
+      </div>
+    </Container>
   );
 }
