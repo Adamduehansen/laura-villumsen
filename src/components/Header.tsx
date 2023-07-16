d075ce08c85d6c342dc7bf15d2e094af5df1b14f
Adam Due Hansen <adamduehan@gmail.com>
2023-07-16 13:12:20 +0200

    Navigation burger menu can now be toggled.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,35 @@
 import Link from 'next/link';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { PageDataContext } from './PageDataProvider';
 import Image from 'next/image';
 import Container from './Container';
+import Navigation from './Navigation';
 
 export default function Header(): JSX.Element {
+  const [showBurgerMenu, setshowBurgerMenu] = useState(false);
+
   const { siteName, subName } = useContext(PageDataContext);
 
+  function toggleBurgerMenu(): void {
+    setshowBurgerMenu((current) => !current);
+  }
+
   return (
-    <Container as='header'>
-      <div className='flex justify-between items-center mt-4 mb-6'>
-        <Link href='/' className='block'>
-          <div>{siteName}</div>
-          <div>{subName}</div>
-        </Link>
-        <button className='w-5 h-5'>
-          <Image src='add.svg' alt='' width={20} height={20} />
-        </button>
+    <header className='fixed inset-x-0 flex flex-col h-screen'>
+      <div className='bg-white'>
+        <Container>
+          <div className='flex justify-between items-center pt-4 pb-6'>
+            <Link href='/' className='block'>
+              <div>{siteName}</div>
+              <div>{subName}</div>
+            </Link>
+            <button onClick={toggleBurgerMenu} className='w-5 h-5'>
+              <Image src='add.svg' alt='' width={20} height={20} />
+            </button>
+          </div>
+        </Container>
       </div>
-    </Container>
+      <div className='flex-1'>{showBurgerMenu && <Navigation />}</div>
+    </header>
   );
 }
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,21 +1,26 @@
 import { useContext } from 'react';
 import { PageDataContext } from './PageDataProvider';
 import Link from 'next/link';
+import Container from './Container';
+import Row from './Row';
+import Col from './Col';
 
 export default function Navigation(): JSX.Element {
   const pageData = useContext(PageDataContext);
 
   return (
-    <nav>
-      <ul>
-        {pageData.navigationItems.map(({ id, text, url }): JSX.Element => {
-          return (
-            <li key={id}>
-              <Link href={url}>{text}</Link>
-            </li>
-          );
-        })}
-      </ul>
+    <nav className='bg-[#E8E6E7] h-full'>
+      <Container>
+        <Row>
+          {pageData.navigationItems.map(({ id, text, url }): JSX.Element => {
+            return (
+              <Col key={id}>
+                <Link href={url}>{text}</Link>
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
     </nav>
   );
 }
