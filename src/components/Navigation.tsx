beaa1016182799c6519f0bfe818197ebe571800e
Adam Due Hansen <adamduehan@gmail.com>
2023-07-22 19:04:16 +0200

    Adds rest of text size in navigation.

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -29,7 +29,7 @@ export default function Navigation({ show }: Props): JSX.Element {
               <li key={id}>
                 <Link
                   href={url}
-                  className='block break-words font-owners-wide text-[5rem] uppercase max-[375px]:text-[4.375rem] md:text-[8.1rem]'
+                  className='block break-words font-owners-wide text-[5rem] uppercase max-[375px]:text-[4.375rem] md:text-[8.1rem] lg:text-[3.9rem] xl:text-[4.9rem] 2xl:text-[5.3rem]'
                 >
                   {text}
                 </Link>
