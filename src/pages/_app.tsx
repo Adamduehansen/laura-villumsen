7dc1185f7dacfe32c6b20eb50525f88695c207a4
Adam Due Hansen <adamduehan@gmail.com>
2023-07-22 20:45:16 +0200

    Adds title element.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,16 @@ import PageDataProvider from '@/components/PageDataProvider';
 import '@/styles/globals.css';
 import { PageProps } from '@/utils/models';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps<PageProps>) {
   return (
     <>
+      <Head>
+        <title>
+          {pageProps.title} | {pageProps.siteName}
+        </title>
+      </Head>
       <PageDataProvider pageData={pageProps}>
         <Header />
         <main>
