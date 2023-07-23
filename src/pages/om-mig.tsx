080a0e30f829ce74310303a833b248536b3802d5
Adam Due Hansen <adamduehan@gmail.com>
2023-07-23 15:58:30 +0200

    Adds om-mig styles for lg.

--- src/pages/om-mig.tsx
+++ src/pages/om-mig.tsx
@@ -14,36 +14,40 @@ export default function About(props: PageData): JSX.Element {
   const cvElements = root.querySelectorAll(':scope > figure');
 
   return (
-    <div className='pt-main'>
-      <p className='text-[2rem] leading-9 mx-grid mb-5'>
-        {textElement!.innerHTML}
-      </p>
-      {props.image && (
-        <Image
-          src={props.image.src}
-          alt={props.image.alt}
-          width={props.image.width}
-          height={props.image.height}
-          className='mb-5'
-        />
-      )}
-      {cvElements.map((cvElement, index): JSX.Element => {
-        const figcaptionElement = cvElement.querySelector('figcaption')!;
-        const title = figcaptionElement.textContent;
-        figcaptionElement.remove();
+    <div className='pt-main lg:grid lg:grid-cols-12'>
+      <div className='lg:flex lg:flex-col lg:col-span-7'>
+        <p className='text-[2rem] leading-9 mx-grid mb-5 lg:order-2'>
+          {textElement!.innerHTML}
+        </p>
+        {props.image && (
+          <Image
+            src={props.image.src}
+            alt={props.image.alt}
+            width={props.image.width}
+            height={props.image.height}
+            className='mb-5 lg:order-1'
+          />
+        )}
+      </div>
+      <div className='lg:col-span-3 lg:col-start-9'>
+        {cvElements.map((cvElement, index): JSX.Element => {
+          const figcaptionElement = cvElement.querySelector('figcaption')!;
+          const title = figcaptionElement.textContent;
+          figcaptionElement.remove();
 
-        return (
-          <div key={index} className='mx-grid'>
-            <h3>{title}</h3>
-            <figure
-              className='mb-5 cvlist'
-              dangerouslySetInnerHTML={{
-                __html: cvElement.innerHTML,
-              }}
-            ></figure>
-          </div>
-        );
-      })}
+          return (
+            <div key={index} className='mx-grid'>
+              <h3>{title}</h3>
+              <figure
+                className='mb-5 cvlist'
+                dangerouslySetInnerHTML={{
+                  __html: cvElement.innerHTML,
+                }}
+              ></figure>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
