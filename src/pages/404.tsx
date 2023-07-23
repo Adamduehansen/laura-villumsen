f960b9acf0854203d4c96a38f92bcf250c49a2b8
Adam Due Hansen <adamduehan@gmail.com>
2023-07-23 14:36:21 +0200

    Adds some styles to 404 page.

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,16 @@
+import Container from '@/components/Container';
 import { getPageData } from '@/services/getPageData';
 import { PageData } from '@/utils/models';
 import { GetStaticProps } from 'next';
 
 export default function NotFound(): JSX.Element {
-  return <main>Siden kunne ikke findes!</main>;
+  return (
+    <div className='pt-main'>
+      <Container>
+        <h1>Siden kunne ikke findes!</h1>
+      </Container>
+    </div>
+  );
 }
 
 export const getStaticProps: GetStaticProps<PageData> = async function () {
