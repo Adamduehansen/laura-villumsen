2af404d33057293eb35e9d4893c2ced8765d0295
Adam Due Hansen <adamduehan@gmail.com>
2023-10-08 00:23:49 +0200

    Returns null.

--- src/pages/sitemap.xml.tsx
+++ src/pages/sitemap.xml.tsx
@@ -1,14 +1,13 @@
 import { wordPressClient } from '@/services/WordPressClient';
 import { GetServerSideProps } from 'next';
 
-export default function Sitemap(): JSX.Element {
-  return <></>;
+export default function Sitemap(): JSX.Element | null {
+  return null;
 }
 
 export const getServerSideProps: GetServerSideProps = async function ({ res }) {
   const { data } = await wordPressClient.getSitemapData();
 
-  // We generate the XML sitemap with the posts data
   const sitemap = `
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       ${data
@@ -24,7 +23,6 @@ export const getServerSideProps: GetServerSideProps = async function ({ res }) {
   `;
 
   res.setHeader('Content-Type', 'text/xml');
-  // we send the XML to the browser
   res.write(sitemap);
   res.end();
 
