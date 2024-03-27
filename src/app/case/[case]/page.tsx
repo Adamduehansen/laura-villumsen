06ae720a1de0582f37c92c7b14ef6947be015ae9
Adam Due Hansen <adamduehan@gmail.com>
2024-03-27 07:40:19 +0100

    Increases size of text.

--- src/app/case/[case]/page.tsx
+++ src/app/case/[case]/page.tsx
@@ -37,7 +37,7 @@ export default async function Page(context: PageContext) {
       <Container>
         <Row className='pb-24'>
           <Col lg={11}>
-            <p className='text-2xl lg:text-4xl'>{work.text}</p>
+            <p className='text-2xl lg:text-5xl'>{work.text}</p>
           </Col>
         </Row>
         <Row className='gap-y-10 pb-32 lg:pb-24'>
