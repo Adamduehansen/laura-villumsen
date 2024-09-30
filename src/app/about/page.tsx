6bfe9c590a4dfd6e56527efe9e0b47e4f0f6ac2b
Adam Due Hansen <adamduehan@gmail.com>
2024-09-30 20:37:18 +0200

    feat(about): adds more space.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,11 @@
-import Col from '@/components/Col';
-import Container from '@/components/Container';
-import Row from '@/components/Row';
-import { wordPressClient } from '@/services/WordPressClient';
-import { Metadata } from 'next';
-import Image from 'next/image';
+import Col from "@/components/Col";
+import Container from "@/components/Container";
+import Row from "@/components/Row";
+import { wordPressClient } from "@/services/WordPressClient";
+import { Metadata } from "next";
+import Image from "next/image";
 
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
 export async function generateMetadata(): Promise<Metadata> {
@@ -22,17 +22,17 @@ export default async function Page() {
 
   return (
     <>
-      <Container className='mb-28 lg:mt-32'>
+      <Container className="mb-28 lg:mt-32">
         <Row>
           <Col>
-            <p className='text-2xl lg:text-[55px] lg:leading-[63px]'>
+            <p className="text-2xl lg:text-[55px] lg:leading-[63px]">
               {content.text}
             </p>
           </Col>
         </Row>
       </Container>
       {content.image && (
-        <Container fluid className='mb-36'>
+        <Container fluid className="mb-28">
           <Row>
             <Col>
               <Image
@@ -40,7 +40,7 @@ export default async function Page() {
                 alt={content.image.alt}
                 width={1440}
                 height={653}
-                className='w-full'
+                className="w-full"
               />
             </Col>
           </Row>
@@ -48,32 +48,35 @@ export default async function Page() {
       )}
       <Container>
         <Row>
-          <Col className='mb-16' lg={4}>
-            <p className='mb-2'>{content.educations.title}</p>
+          <Col className="mb-16" lg={4}>
+            <p className="mb-2">{content.educations.title}</p>
             <div
-              className='[&>table>tbody>tr>td]:align-top [&>table>tbody>tr>td]:pb-4 first:[&>table>tbody>tr>td]:w-28 [&>table]:w-full '
+              className="[&>table>tbody>tr>td]:align-top [&>table>tbody>tr>td]:pb-4 first:[&>table>tbody>tr>td]:w-28 [&>table]:w-full "
               dangerouslySetInnerHTML={{
                 __html: content.educations.content,
               }}
-            ></div>
+            >
+            </div>
           </Col>
-          <Col className='mb-16' lg={4}>
-            <p className='mb-2'>{content.employments.title}</p>
+          <Col className="mb-16" lg={4}>
+            <p className="mb-2">{content.employments.title}</p>
             <div
-              className='[&>table>tbody>tr>td]:align-top [&>table>tbody>tr>td]:pb-4 first:[&>table>tbody>tr>td]:w-28 [&>table]:w-full'
+              className="[&>table>tbody>tr>td]:align-top [&>table>tbody>tr>td]:pb-4 first:[&>table>tbody>tr>td]:w-28 [&>table]:w-full"
               dangerouslySetInnerHTML={{
                 __html: content.employments.content,
               }}
-            ></div>
+            >
+            </div>
           </Col>
-          <Col className='mb-16' lg={4}>
-            <p className='mb-2'>{content.awards.title}</p>
+          <Col className="mb-16" lg={4}>
+            <p className="mb-2">{content.awards.title}</p>
             <div
-              className='[&>table>tbody>tr>td]:align-top [&>table>tbody>tr>td]:pb-4 first:[&>table>tbody>tr>td]:w-28 [&>table]:w-full [&>table>tbody>tr>td>a]:underline [&>table>tbody>tr>td>a]:text-link'
+              className="[&>table>tbody>tr>td]:align-top [&>table>tbody>tr>td]:pb-4 first:[&>table>tbody>tr>td]:w-28 [&>table]:w-full [&>table>tbody>tr>td>a]:underline [&>table>tbody>tr>td>a]:text-link"
               dangerouslySetInnerHTML={{
                 __html: content.awards.content,
               }}
-            ></div>
+            >
+            </div>
           </Col>
         </Row>
       </Container>
