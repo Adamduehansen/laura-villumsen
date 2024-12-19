bf35d6cb9eb90a84703be15732f27aaee9ffc364
Adam Due Hansen <adamduehan@gmail.com>
2024-12-19 20:38:31 +0100

    Fix: correct URL to favicon.

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -9,7 +9,7 @@ export default function App({ Component }: PageProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>laura-villumsen</title>
         <link rel="stylesheet" href="/styles.css" />
-        <link rel="shortcut icon" href="favicon.png" type="image/x-icon" />
+        <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
       </head>
       <body>
         <Header />
