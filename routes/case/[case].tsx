8f7bb1f5e14afece297ad4a0d3c1c17df4f86fe5
Adam Due Hansen <adamduehan@gmail.com>
2024-12-19 21:41:25 +0100

    feat: adds description on case pages.

--- routes/case/[case].tsx
+++ routes/case/[case].tsx
@@ -1,18 +1,28 @@
 import { FreshContext } from "$fresh/server.ts";
 import { JSX } from "preact/jsx-runtime";
+import { parse } from "node-html-parser";
+import { getPost } from "../../services/post-service.ts";
 
 export default async function Case(
   _req: Request,
   ctx: FreshContext,
 ): Promise<JSX.Element> {
   const caseSlug = ctx.params.case;
-  const response = await fetch(
-    `https://wp.lauravillumsen.dk/wp-json/wp/v2/posts?slug=${caseSlug}`,
-  );
-  const json = await response.json();
+  const post = await getPost(caseSlug);
+
+  const document = parse(post.content.rendered);
+  const text = document.querySelector("p");
+
+  console.log(text);
+
   return (
     <div>
-      <p>Case: {ctx.params.case}</p>
+      <p
+        dangerouslySetInnerHTML={{
+          __html: text!.innerHTML,
+        }}
+      >
+      </p>
     </div>
   );
 }
