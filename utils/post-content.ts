c42c2a9129f96b7e78cb7cc81468034a417c43d3
Adam Due Hansen <adamduehan@gmail.com>
2024-12-23 07:20:35 +0100

    feat: PostContent can parse video elements.

--- utils/post-content.ts
+++ utils/post-content.ts
@@ -1,11 +1,16 @@
-import { Node, parse } from "node-html-parser";
+import { HTMLElement, Node, NodeType, parse } from "node-html-parser";
 
 interface TextBlock {
   type: "text";
   text: string;
 }
 
-type Block = TextBlock;
+interface VideoBlock {
+  type: "video";
+  src: string;
+}
+
+type Block = TextBlock | VideoBlock;
 
 export class PostContent {
   readonly intro: string;
@@ -14,23 +19,41 @@ export class PostContent {
 
   constructor(rawContent: string) {
     const htmlDocument = parse(rawContent);
-    const [intro, text, ...rest] = htmlDocument.childNodes.filter((node) => {
-      return node.rawTagName !== "";
-    });
+    const [intro, text, ...rest] = htmlDocument.childNodes.filter(
+      this.#isHtmlNode,
+    );
 
     this.intro = intro.innerText;
     this.text = text.innerText;
     this.blocks = this.#getBlocks(rest);
   }
 
-  #getBlocks(nodes: Node[]): Block[] {
-    return nodes.reduce<Block[]>((nodes, node): Block[] => {
-      console.log(node.rawTagName);
+  #isHtmlNode(node: Node): node is HTMLElement {
+    return node.nodeType === NodeType.ELEMENT_NODE;
+  }
 
-      switch (node.rawTagName) {
-        default:
+  #getBlocks(nodes: HTMLElement[]): Block[] {
+    return nodes.reduce<Block[]>((nodes, node): Block[] => {
+      const { classList } = node;
+
+      if (classList.contains("wp-block-video")) {
+        const videoElement = node.querySelector("video");
+        if (videoElement === null) {
+          console.log("Whops, video figure had no video element");
           return nodes;
+        }
+        const videoSrc = videoElement.getAttribute("src");
+        if (videoSrc === undefined) {
+          console.log("Whops, video element had no src");
+          return nodes;
+        }
+        return [...nodes, {
+          type: "video",
+          src: videoSrc,
+        }];
       }
+
+      return nodes;
     }, []);
   }
 }
