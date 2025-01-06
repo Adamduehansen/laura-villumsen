283a029af03bdd6f01a80c6e9b9f7bd329615442
Adam Due Hansen <adamduehan@gmail.com>
2025-01-06 07:22:34 +0100

    feat: new custom field for thumbnails.

--- docker/data/wp-content/themes/blank/functions.php
+++ docker/data/wp-content/themes/blank/functions.php
@@ -97,28 +97,64 @@ add_action('init', 'register_wp_case_info_block');
 /* ===================================================== */
 
 /* THUMBNAIL WITH VIDEO SUPPORT */
-function add_video_thumbnail_meta_box() {
+function add_media_thumbnail_meta_box() {
   add_meta_box(
-      'video_thumbnail_meta_box',
-      'Video Thumbnail',
-      'video_thumbnail_meta_box_callback',
+      'media_thumbnail_meta_box',
+      'Media Thumbnail',
+      'media_thumbnail_meta_box_callback',
       'post',
       'side',
       'low'
   );
 }
-add_action('add_meta_boxes', 'add_video_thumbnail_meta_box');
+add_action('add_meta_boxes', 'add_media_thumbnail_meta_box');
 
-function video_thumbnail_meta_box_callback($post) {
-  $video_url = get_post_meta($post->ID, 'video_thumbnail', true);
-  echo '<label for="video_thumbnail">Video URL:</label>';
-  echo '<input type="url" id="video_thumbnail" name="video_thumbnail" value="' . esc_url($video_url) . '" style="width:100%">';
+function media_thumbnail_meta_box_callback($post) {
+  $media_id = get_post_meta($post->ID, 'media_thumbnail_id', true);
+  $media_url = $media_id ? wp_get_attachment_url($media_id) : '';
+
+  echo '<div id="media-thumbnail-container">';
+  echo $media_url ? '<div><img src="' . esc_url($media_url) . '" style="max-width:100%;"></div>' : '';
+  echo '<input type="hidden" id="media_thumbnail_id" name="media_thumbnail_id" value="' . esc_attr($media_id) . '">';
+  echo '<button type="button" id="select-media-thumbnail" class="button">Select Media</button>';
+  echo '<button type="button" id="remove-media-thumbnail" class="button" style="' . ($media_url ? '' : 'display:none;') . '">Remove Media</button>';
+  echo '</div>';
 }
 
-function save_video_thumbnail_meta($post_id) {
-  if (isset($_POST['video_thumbnail'])) {
-      update_post_meta($post_id, 'video_thumbnail', esc_url_raw($_POST['video_thumbnail']));
+function enqueue_media_library_scripts($hook) {
+  if ('post.php' === $hook || 'post-new.php' === $hook) {
+      wp_enqueue_media();
+      wp_enqueue_script(
+          'media-thumbnail-script',
+          get_template_directory_uri() . '/media-thumbnail.js', // Adjust path if needed
+          array('jquery'),
+          filemtime(get_template_directory() . '/media-thumbnail.js'),
+          true
+      );
   }
 }
-add_action('save_post', 'save_video_thumbnail_meta');
+add_action('admin_enqueue_scripts', 'enqueue_media_library_scripts');
+
+function save_media_thumbnail_meta($post_id) {
+  if (isset($_POST['media_thumbnail_id'])) {
+      update_post_meta($post_id, 'media_thumbnail_id', absint($_POST['media_thumbnail_id']));
+  }
+}
+add_action('save_post', 'save_media_thumbnail_meta');
+
+function add_media_thumbnail_to_rest($data, $post, $context) {
+  $media_id = get_post_meta($post->ID, 'media_thumbnail_id', true);
+
+  if ($media_id) {
+      $data->data['media_thumbnail'] = array(
+          'id' => $media_id,
+          'url' => wp_get_attachment_url($media_id),
+          'type' => wp_check_filetype(wp_get_attachment_url($media_id))['type'],
+      );
+  }
+
+  return $data;
+}
+add_filter('rest_prepare_post', 'add_media_thumbnail_to_rest', 10, 3);
+
 /* ===================================================== */
