2bbeb6f583a4ee88bb5ef7088447d40fb5cdf9b2
Adam Due Hansen <adamduehan@gmail.com>
2025-01-07 07:27:46 +0100

    feat: adds figure wrapper to images.

--- components/image.tsx
+++ components/image.tsx
@@ -3,11 +3,11 @@ import { JSX } from "preact/jsx-runtime";
 
 export function Image(props: JSX.IntrinsicElements["img"]): JSX.Element {
   return (
-    <>
+    <figure>
       <Head>
         <link rel="preload" as="image" href={props.src} />
       </Head>
       <img {...props} decoding="async" />
-    </>
+    </figure>
   );
 }
