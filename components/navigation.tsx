0c2999384c2d2fd95e9b2ab21f90eac705d693cb
Adam Due Hansen <adamduehan@gmail.com>
2025-01-08 08:11:45 +0100

    chore: adds documentation for current page.

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -2,14 +2,15 @@ import { JSX } from "preact/jsx-runtime";
 
 export function Navigation(): JSX.Element {
   // TODO: set styles for current list item.
+  // https://fresh.deno.dev/docs/examples/active-links
   return (
     <nav class="flex-1 flex items-center">
       <ul class="text-4xl">
         <li>
-          <a href="work">Work</a>
+          <a href="/">Work</a>
         </li>
         <li>
-          <a href="about">About</a>
+          <a href="/about">About</a>
         </li>
       </ul>
     </nav>
