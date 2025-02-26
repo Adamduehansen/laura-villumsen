a3b0ebf4e5f8dc97f94220d08a5211ad89f9b7b2
Adam Due Hansen <adamduehan@gmail.com>
2025-02-26 13:50:27 +0100

    Update get-posts.ts

--- services/post/get-posts.ts
+++ services/post/get-posts.ts
@@ -11,6 +11,8 @@ const WpSiteHost = Deno.env.get("WP_SITE_URL");
 export class FetchGetPostsHandler implements GetPostsHandler {
   async getPosts() {
     const response = await fetch(`${WpSiteHost}/wp-json/wp/v2/posts`);
+    const text = await respose.text();
+    console.log(text);
     const json = await response.json();
     return v.parse(v.array(PostSchema), json);
   }
