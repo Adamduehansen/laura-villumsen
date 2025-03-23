236ebb947075e55b2f4ab275e5c26fa9006606fe
Adam Due Hansen <adamduehan@gmail.com>
2025-03-23 22:49:39 +0100

    Adds robots.txt file.

--- fresh.config.ts
+++ fresh.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "$fresh/server.ts";
 import tailwind from "$fresh/plugins/tailwind.ts";
 import { sitemapPlugin } from "$plugins/sitemap.ts";
 
-// TODO: add robots.txt.
 // TODO: add open graph and twitter cards
 
 export default defineConfig({
