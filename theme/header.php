c5b090a9fde1db655e0ef5234af63b81048a870f
Adam Due Hansen <adamduehan@gmail.com>
2026-01-10 12:25:15 +0100

    uses correct font.

--- theme/header.php
+++ theme/header.php
@@ -7,6 +7,7 @@
 <head>
   <meta charset=”<?php bloginfo( 'charset' ); ?>”>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://use.typekit.net/wsq1sgw.css" />
   <?php wp_head(); ?>
   <title><?php echo $pageTitle ?></title>
 </head>
