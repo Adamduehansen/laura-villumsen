f32ba8dd40986cb655c9224cb0d5064412917cf5
Adam Due Hansen <adamduehan@gmail.com>
2026-01-17 13:36:02 +0100

    Applies first set of styles for the case page.

--- theme/single.php
+++ theme/single.php
@@ -9,23 +9,29 @@ class CaseInfo {
 
 class CaseInfoRenderer {
   function render($caseInfo) {
-    $caseInfoHtml = "";
+    $caseInfoHtml = "<div class='grid-container'>";
     $caseInfoHtml .= $this->append("Client", $caseInfo->client);
     $caseInfoHtml .= $this->append("Year", $caseInfo->date);
     $caseInfoHtml .= $this->append("Services", $caseInfo->tags);
     $caseInfoHtml .= $this->append(null, $caseInfo->website);
     $caseInfoHtml .= $this->append("Notes", $caseInfo->notes);
+    $caseInfoHtml .= "</div>";
 
     return $caseInfoHtml;
   }
 
-  private function get($title, $value) {
+  private function render_info($title, $value) {
     $lowerTitle = strtolower($title);
     
     if ($lowerTitle == "year") {
       return substr($value, 0, 4);
     } else if (is_array($value)) {
-      return $value;
+      $html = "<ul>";
+      foreach ($value as $item) {
+        $html .= "<li>" . (is_object($item) ? $item->name : $item) . "</li>";
+      }
+      $html .= "</ul>";
+      return $html;
     } else if (filter_var($value, FILTER_VALIDATE_URL)) {
       return "<a href='" . $value . "' target='_BLANK' rel='noopener noreferrer'>Visit website here</a>";
     } else {
@@ -38,10 +44,10 @@ class CaseInfoRenderer {
       return "";
     }
 
-    $heading = $title ? "<p>" . $title . "</p>" : null;
-    $text = $this->get($title, $value);
+    $heading = $title ? "<p class='font-semibold'>" . $title . "</p>" : null;
+    $text = "<p>" . $this->render_info($title, $value) . "</p>";
 
-    return "<div>" . ($heading) . $text . "</div>";
+    return "<div class='grid-item'>" . ($heading) . $text . "</div>";
   }
 }
 
