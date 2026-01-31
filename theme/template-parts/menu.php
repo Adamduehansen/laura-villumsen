1594352b074f4dff72e20c8580aad593cb0b1479
Adam Due Hansen <adamduehan@gmail.com>
2026-01-31 14:39:17 +0100

    A minor setback.

--- theme/template-parts/menu.php
+++ theme/template-parts/menu.php
@@ -3,7 +3,7 @@ $menu_items = wp_get_nav_menu_items('navigation');
 ?>
 
 <nav>
-  <ul>
+  <ul class="md-menu closed">
     <?php foreach ($menu_items as $item) { ?>
       <li>
         <a href="<?php echo esc_url($item->url) ?>"><?php echo esc_html($item->title) ?></a>
